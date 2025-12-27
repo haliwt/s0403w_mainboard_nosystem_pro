104f40ffd5476c6c1319d27ef09e2406e47e7fad
haliwt <jonewang@126.com>
2025-12-27 11:36:31 +0800

    update

--- Bsp/Src/bsp_adc.c
+++ Bsp/Src/bsp_adc.c
@@ -21,7 +21,7 @@ volatile uint8_t adc_conversion_complete = 0;
 
 uint16_t fan_detect_voltage = 1000;
 uint16_t ptc_detect_voltage;
-
+uint8_t counter_error;
 
 static uint16_t compute_voltage(uint16_t raw_value) ;
 static uint16_t ADC_FAN_ReadVoltage(void);
@@ -41,7 +41,7 @@ uint16_t mean_fan_buf[SAMPLE_COUNT];
 **********************************************************************/
 void adc_detected_hundler(void)
 {
-    static uint8_t counter_error;
+    
     if(gctl_t.gTimer_fan_adc_times > 4 && gpro_t.stopTwoHours_flag ==0 && gpro_t.fan_warning_flag==0){ //detected 3 times is 60s 
         gctl_t.gTimer_fan_adc_times =0;
         Fan_Full_Speed();
@@ -53,11 +53,11 @@ void adc_detected_hundler(void)
 
 	   if(fan_detect_voltage < 420){
 
-	      if(fan_detect_voltage > 370 && fan_detect_voltage < 400){
- 
+//	      if(fan_detect_voltage > 370 && fan_detect_voltage < 400){
+// 
 
-		  }
-		  else{
+//		  }
+//		  else{
 		  	  counter_error ++;
 			  if(counter_error > 5){
 			      gpro_t.fan_warning_flag=1;
@@ -66,7 +66,7 @@ void adc_detected_hundler(void)
 				  ptc_recoder_flag = 0; //WT.EDIT 2025.11.17
 				  PTC_SetLow();
 			  }
-		  }
+		  //}
 
 	   }
 	   else{
