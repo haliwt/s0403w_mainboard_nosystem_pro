9b9aaad570a9a816dd6273631310f2158fe85597
haliwt <jonewang@126.com>
2026-01-06 10:28:11 +0800

    update be used static allocation RAM .DMA+IDLE DATA.2026.01.06

--- Inc/FreeRTOSConfig.h
+++ Inc/FreeRTOSConfig.h
@@ -61,9 +61,9 @@
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
-#define configMAX_PRIORITIES                     ( 4 )
+#define configMAX_PRIORITIES                     ( 3 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)4*1024)//3*1024
+#define configTOTAL_HEAP_SIZE                    0//((size_t)4*1024)//3*1024
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
--- Src/tim.c
+++ Src/tim.c
@@ -54,7 +54,7 @@ void MX_TIM1_Init(void)
   TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_PWM1;
   TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_DISABLE;
   TIM_OC_InitStruct.OCNState = LL_TIM_OCSTATE_DISABLE;
-  TIM_OC_InitStruct.CompareValue = 0;
+  TIM_OC_InitStruct.CompareValue = 20;
   TIM_OC_InitStruct.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
   TIM_OC_InitStruct.OCNPolarity = LL_TIM_OCPOLARITY_HIGH;
   TIM_OC_InitStruct.OCIdleState = LL_TIM_OCIDLESTATE_LOW;
