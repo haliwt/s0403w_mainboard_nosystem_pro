cf984b4abf18acce71c52a482c2feb670aab1cc3
haliwt <jonewang@126.com>
2026-01-29 21:44:20 +0800

    update

--- Bsp/Inc/bsp.h
+++ Bsp/Inc/bsp.h
@@ -68,7 +68,7 @@
 #include "bsp_subscription.h"
 
 
-#define Enable_EventRecorder  1
+#define Enable_EventRecorder  0
 
 #define DEBUG_FLAG        Enable_EventRecorder //(Enable_EventRecorder? 1:0)
 
